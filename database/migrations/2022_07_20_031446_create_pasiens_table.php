dac91ee645ee3d81ac61c6156571e1a813700fda
Aurasalsabila <aurasalsabila288@gmail.com>
2022-07-20 10:40:53 +0700

    buat login dan register

---
+++ database/migrations/2022_07_20_031446_create_pasiens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('jenis');
+            $table->string('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
---
+++ database/migrations/2022_07_20_031545_create_rekmeds_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rekmeds', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_rekmed')->unique();
+            $table->string('nik');
+            $table->date('tanggal_berobat');
+            $table->string('dianogsa');
+            $table->string('kode_obat');
+            $table->string('transaksi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rekmeds');
+    }
+};
